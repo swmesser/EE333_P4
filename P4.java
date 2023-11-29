ad8d0608526b1ff8c9a3a3cd755e64c5f91533ea
swmesser <samwade1022@gmail.com>
2023-11-29 01:00:46 -0600

    Init commit of P4 Skeleton Class

---
+++ P4.java
@@ -0,0 +1,17 @@
+package OMS;
+
+/* 
+ * File:  P4
+ * Copy: Copyright (c) 2023 Samuel W. Messer
+ * BlazerID: swmesser
+ * Vers: 1.0.0 Nov 29, 2023 SWM - Original Coding
+ * Desc: Driver for testing concepts
+ */
+
+public class P4 {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+
+}
